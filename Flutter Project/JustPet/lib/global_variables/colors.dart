655dae4a1a23da750a6be8719480e9c7c3768a14
Antonio Zizzari <72402942+Antonio-Zizzari@users.noreply.github.com>
2022-11-21 11:21:33 +0100

    Delete Flutter Project/JustPet/lib directory

--- Flutter Project/JustPet/lib/global_variables/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF05780C);
-const ksecondaryColor = Color(0xFFB5BFD0);
-const kTextColor = Color(0xFF50505D);
-const kErrorColor = Color(0xFFF03738);
-const kTextLightColor = Color(0xFF6A727D);
